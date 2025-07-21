b22316d9249fbb7837d4ae9584e1ca36a9c5999e
waie-ya <waie.ya.main@gmail.com>
2025-07-22 03:28:13 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        await Task.Delay(3000);
+        Console.WriteLine("Finish");
+    }
+}
